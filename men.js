3e36c49bb497a9085d6185e754e696977bf0c4dc
AbhayKholia <103960752+AbhayKholia@users.noreply.github.com>
2022-07-24 10:24:07 +0530

    done

---
+++ men.js
@@ -0,0 +1,325 @@
+products_data1 = [ {
+    name:"JACQUEMUS",
+    brand:"Thalie Burnt Orange leather clogs",
+    price:"$204-$775",
+    catagory:"Compare 21 store",
+    productID:"men1",
+    img:"https://cdn.modesens.com/availability/39417845?w=400&",
+    },
+        {
+    name:"ISABEL MARANT",
+    brand:"Thalie Burnt Orange leather clogs  ",
+    price:"$119 - $460",
+    catagory:"Compare 16 store",
+    productID:"men2",
+    img:"https://cdn.modesens.com/availability/28287241?w=400&",
+    },
+      {
+    name:"VERSACE ",
+    brand:"Barocco Paint High_Waised",
+    price:"$193-$575",
+    catagory:"Compare 18 store",
+    productID:"men3",
+    img:"https://cdn.modesens.com/product/12618509_255?w=400&",
+    },
+        {
+    name:"BALENCIAGA",
+    brand:"Piscine Logo-embosed rubber",
+    price:"$162-$445",
+    catagory:"Compare 18 store",
+    productID:"men4",
+    img:"https://cdn.modesens.com/availability/45111614?w=400&",
+    },
+        {
+    name:"ISABEL",
+    brand:"Nadege Flore-pant Mid-rise",
+    price:"$118-$590",
+    catagory:"Compare 18 store",
+    productID:"men5",
+    img:"https://cdn.modesens.com/product/29236971_5?w=400&",
+    },
+        {
+    name:"BALENCIAGE",
+    brand:"Piscine Logo-embosed rubber",
+    price:"$370-$773",
+    catagory:"Compare 20 store",
+    productID:"men6",
+    img:"https://res.cloudinary.com/ssenseweb/image/upload/b_white%2Cc_lpad%2Cg_center%2Ch_960%2Cw_960/c_scale%2Ch_680/f_auto%2Cdpr_1.0/202418M202185_1.jpg",
+    },
+        {
+    name:"ZIMMERMANN",
+    brand:"Pattie Floral Wrap Tiered Three",
+    price:"$577-$744",
+    catagory:"Compare 22 store",
+    productID:"men7",
+    img:"https://cdn.modesens.com/product/43660582_1?w=400&",
+    },
+        {
+    name:"BALENCIAGA",
+    brand:"+Crocs madame Perforated",
+    price:"$485-$856",
+    catagory:"Compare 16 store",
+    productID:"men8",
+    img:"https://res.cloudinary.com/ssenseweb/image/upload/b_white%2Cc_lpad%2Cg_center%2Ch_960%2Cw_960/c_scale%2Ch_680/f_auto%2Cdpr_1.0/212278M231014_1.jpg",
+    },
+        {
+    name:"ATTICO",
+    brand:"Audrey Open Back Sequinned Mini ",
+    price:"$571-$1280",
+    catagory:"Compare 23 store",
+    productID:"men9",
+    img:"https://img.zolaprod.babsta.net/MyFjYVenpY6qxaxk5o8818o-0eg=/500x500/d5e511ebc766439dae869a7a60a126b9/product.JAC8M8THORA7AAA100.NBG",
+    },
+       {
+    name:"BALENCIAGA",
+    brand:"Piscine Logo-embosed rubber",
+    price:"$620-$1225",
+    catagory:"Compare 14 store",
+    productID:"men10",
+    img:"https://cdn.modesens.com/product/18523816_154?w=400&",
+    },
+        {
+    name:"VERSACE",
+    brand:"Baroque-pant Racerback Sports",
+    price:"$153-$425",
+    catagory:"Compare 14 store",
+    productID:"men11",
+    img:"https://cdn.modesens.com/availability/49560130?w=400&",
+    }, 
+     {
+    name:"TOTEME",
+    brand:"La Robe Limao Cutout Halter Neck Minidress In",
+    price:"$204-$775",
+    catagory:"Compare 21 store",
+    productID:"men12",
+    img:"https://cdn.modesens.com/availability/48045018?w=400&",
+    },
+        {
+    name:"JACQUEMUS",
+    brand:"Thalie Burnt Orange leather clogs  ",
+    price:"$119 - $460",
+    catagory:"Compare 16 store",
+    productID:"men13",
+    img:"https://cdn.modesens.com/availability/46004022?w=400&",
+    },
+        {
+    name:"ISABEL MARANT ",
+    brand:"Barocco Paint High_Waised",
+    price:"$193-$575",
+    catagory:"Compare 18 store",
+    productID:"men14",
+    img:"https://cdn.modesens.com/availability/49626553?w=400&",
+    },
+        {
+    name:"JACQUEMUS",
+    brand:"Piscine Logo-embosed rubber",
+    price:"$162-$445",
+    catagory:"Compare 18 store",
+    productID:"men15",
+    img:"https://cdn.modesens.com/product/21233692_255?w=400&",
+    },
+        {
+    name:"ZIMMERMANN",
+    brand:"Nadege Flore-pant Mid-rise",
+    price:"$118-$590",
+    catagory:"Compare 18 store",
+    productID:"men16",
+    img:"https://cdn.modesens.com/product/18871612_223?w=400&",
+    },
+        {
+    name:"ZIMMERMANN",
+    brand:"Piscine Logo-embosed rubber",
+    price:"$370-$773",
+    catagory:"Compare 20 store",
+    productID:"men17",
+    img:"https://cdn.modesens.com/availability/49243162?w=400&",
+    },
+        {
+    name:"ISABEL MARANT",
+    brand:"Pattie Floral Wrap Tiered Three",
+    price:"$577-$744",
+    catagory:"Compare 22 store",
+    productID:"men18",
+    img:"https://cdn.modesens.com/availability/30460695?w=400&",
+    },
+        {
+    name:"ZIMMERMANN",
+    brand:"+Crocs madame Perforated",
+    price:"$485-$856",
+    catagory:"Compare 16 store",
+    productID:"men19",
+    img:"https://cdn.modesens.com/product/40016256_4?w=400&",
+    },
+        {
+    name:"ZIMMERMANN",
+    brand:"Audrey Open Back Sequinned Mini ",
+    price:"$571-$1280",
+    catagory:"Compare 23 store",
+    productID:"men20",
+    img:"https://cdn.modesens.com/availability/53271026?w=400&",
+    },
+       {
+    name:"VERSACE",
+    brand:"Piscine Logo-embosed rubber",
+    price:"$620-$1225",
+    catagory:"Compare 14 store",
+    productID:"men21",
+    img:"https://res.cloudinary.com/ssenseweb/image/upload/b_white%2Cc_lpad%2Cg_center%2Ch_960%2Cw_960/c_scale%2Ch_680/f_auto%2Cdpr_1.0/221404M193006_1.jpg",
+    },
+        {
+    name:"JACQUEMUS",
+    brand:"Baroque-pant Racerback Sports",
+    price:"$153-$425",
+    catagory:"Compare 14 store",
+    productID:"men22",
+    img:"https://cdn.modesens.com/product/31814674_30?w=400&",
+    },
+      {
+    name:"JACQUEMUS",
+    brand:"La Robe Limao Cutout Halter Neck Minidress In",
+    price:"$204-$775",
+    catagory:"Compare 21 store",
+    productID:"men23",
+    img:"https://cdn.modesens.com/availability/33767572?w=400&",
+    },
+        {
+    name:"ISABEL MARANT",
+    brand:"Thalie Burnt Orange leather clogs  ",
+    price:"$119 - $460",
+    catagory:"Compare 16 store",
+    productID:"men24",
+    img:"https://cdn.modesens.com/product/39089391_15?w=400&",
+    },
+        {
+    name:"VERSACE ",
+    brand:"Barocco Paint High_Waised",
+    price:"$193-$575",
+    catagory:"Compare 18 store",
+    productID:"men25",
+    img:"https://cdn.modesens.com/product/39096375_3?w=400&",
+    },
+        {
+    name:"BALENCIAGA",
+    brand:"Piscine Logo-embosed rubber",
+    price:"$162-$445",
+    catagory:"Compare 18 store",
+    productID:"men26",
+    img:"https://cdn.modesens.com/availability/27363819?w=400&",
+    },
+        {
+    name:"ISABEL",
+    brand:"Nadege Flore-pant Mid-rise",
+    price:"$118-$590",
+    catagory:"Compare 18 store",
+    productID:"men27",
+    img:"https://cdn.modesens.com/product/39241395_5?w=400&",
+    },
+        {
+    name:"BALENCIAGE",
+    brand:"Piscine Logo-embosed rubber",
+    price:"$370-$773",
+    catagory:"Compare 20 store",
+    productID:"men28",
+    img:"https://cdn.modesens.com/availability/46241343?w=400&",
+    },
+        {
+    name:"ZIMMERMANN",
+    brand:"Pattie Floral Wrap Tiered Three",
+    price:"$577-$744",
+    catagory:"Compare 22 store",
+    productID:"men29",
+    img:"https://cdn.modesens.com/availability/46241343?w=400&",
+    },
+        {
+    name:"BALENCIAGA",
+    brand:"+Crocs madame Perforated",
+    price:"$485-$856",
+    catagory:"Compare 16 store",
+    productID:"men30",
+    img:"https://cdn-images.farfetch-contents.com/17/33/47/61/17334761_36299358_1000.jpg",
+    },
+        {
+    name:"ATTICO",
+    brand:"Audrey Open Back Sequinned Mini ",
+    price:"$571-$1280",
+    catagory:"Compare 23 store",
+    productID:"men31",
+    img:"https://cdn.modesens.com/product/11118030_148?w=400&",
+    },
+       {
+    name:"BALENCIAGA",
+    brand:"Piscine Logo-embosed rubber",
+    price:"$620-$1225",
+    catagory:"Compare 14 store",
+    productID:"men32",
+    img:"https://cdn.modesens.com/availability/48974212?w=400&",
+    },
+     
+    ]
+     
+    localStorage.setItem("masai-ecomm1",JSON.stringify(products_data1))
+    
+    let cartLSdata= JSON.parse(localStorage.getItem("masai-cart")) || []
+     let products1 = document.querySelector(".products1")
+      
+     var Filter=document.querySelector("#sort1").value
+    
+     function  display(data1){
+      products1.innerHTML=null;
+    
+      data1.forEach(function(el,){
+        var card = document.createElement("div")
+         card.setAttribute("class","card1")
+        var img = document.createElement("img")
+        img.src=el.img
+    
+        var title =document.createElement("h2")
+        title.textContent= el.name
+    
+        var desc = document.createElement("P")
+        desc.textContent=el.brand
+    
+        var price = document.createElement("h5")
+        price.textContent=el.price
+        
+    
+        var cato = document.createElement("P")
+        cato.textContent=el.catagory
+    
+        var button = document.createElement("button")
+        button.setAttribute("class","btn")
+        button.innerText="Quick View"
+        button.addEventListener("click",function(){
+         QuickView(el)
+        })
+    
+        card.append(img,title,desc,price,cato,button)
+        products1.append(card)
+           
+      })
+     }
+    
+    display(products_data1)  
+    console.log(products_data1)
+
+
+     Filter.addEventListener("change",function(){
+      if(Filter== "Lowest price" ){
+        display(products_data1)
+      }else{
+        var filtered = products_data1.sort(function(a,b){
+          if(a.price>b.price)  return 1
+          if(a.price<b.price)  return -1 
+          return 0
+        })
+           console.log(filtered)
+        display(filtered)
+      }
+     })
+
+     function QuickView(el ){
+      cartLSdata.push(el)
+      localStorage.setItem("masai-cart",JSON.stringify(cartLSdata))
+      alert("product Add to cart") 
+    // window.location.href="abhay.html"
+     }
